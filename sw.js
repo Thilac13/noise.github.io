a531c05e296062456b078c098ff6ee121bcb0ae6
Thilac13 <89009219+Thilac13@users.noreply.github.com>
2025-05-05 03:41:10 +0300

    PWA

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'noise-generator-v1';
+const ASSETS = [
+  '/noise_1.0.0.html',
+  '/styles.css',
+  '/script.js',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
